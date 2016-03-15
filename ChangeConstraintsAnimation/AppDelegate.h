ff0d1bae1dfb331cd5f4cf3f1b5461c2789bd85e
chengxun <chengxun@xiaoshouyi.com>
2016-03-15 09:13:07 +0800

    Initial Commit

---
+++ ChangeConstraintsAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeConstraintsAnimation
+//
+//  Created by chengxun on 16/3/15.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
