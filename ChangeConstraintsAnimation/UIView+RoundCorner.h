3dba8f3cc89f7656540cbdecc427ab929a98f28c
chengxun <chengxun@xiaoshouyi.com>
2016-03-15 15:09:37 +0800

    增加UIView的切圆角

---
+++ ChangeConstraintsAnimation/UIView+RoundCorner.h
@@ -0,0 +1,13 @@
+//
+//  UIView+RoundCorner.h
+//  ChangeConstraintsAnimation
+//
+//  Created by chengxun on 16/3/15.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (RoundCorner)
+
+@end
---
+++ ChangeConstraintsAnimation/UIView+RoundCorner.m
@@ -0,0 +1,13 @@
+//
+//  UIView+RoundCorner.m
+//  ChangeConstraintsAnimation
+//
+//  Created by chengxun on 16/3/15.
+//  Copyright © 2016年 chengxun. All rights reserved.
+//
+
+#import "UIView+RoundCorner.h"
+
+@implementation UIView (RoundCorner)
+
+@end
